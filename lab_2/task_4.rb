a1d6b3d320c30e5470f1e510c660c334554299de
DzOlha <monsarda11@gmail.com>
2022-09-10 19:29:55 +0300

    Completed the FOURTH task of the 2nd lab work!

---
+++ lab_2/task_4.rb
@@ -0,0 +1,20 @@
+def convertToBinarySystem(num)
+    x = num.divmod(2)
+    arr = []
+    i = 0
+    while (x[0] > 0) do
+        arr[i] = x[1]
+        print x, "\n"
+        x = x[0].divmod(2)
+        i += 1
+        if(x[0] == 0) then 
+            arr[i] = x[1]
+            print x, "\n"
+        end
+    end
+    return arr.reverse.join.to_i
+end
+
+number = 451
+print "Result = ", convertToBinarySystem(number)
+
